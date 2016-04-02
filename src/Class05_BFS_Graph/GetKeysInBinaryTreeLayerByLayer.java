616a402bf0bb2beb41bdf783804aed939d0d1437
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-04-01 21:23:53 -0400

    print tree

---
+++ src/Class05_BFS_Graph/GetKeysInBinaryTreeLayerByLayer.java
@@ -0,0 +1,58 @@
+package Class05_BFS_Graph;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/* Get the list of list of keys in a given binary tree 
+ * layer by layer. Each layer is represented by a list 
+ * of keys and the keys are traversed from left to right.
+ * Examples
+ *      			 5
+
+			      /    \
+			
+			    3        8
+			
+			  /   \        \
+			
+			 1     4        11
+ * the result is [ [5], [3, 8], [1, 4, 11] ]
+ * 
+ * Corner Cases:
+ * What if the binary tree is null? 
+ * Return an empty list of list in this case.
+ * 
+ * How is the binary tree represented?
+ * We use the level order traversal sequence with a 
+ * special symbol "#" denoting the null node.
+ * */
+public class GetKeysInBinaryTreeLayerByLayer {
+	public List<List<Integer>> layerByLayer(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return result;
+		}
+		Deque<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> curLayer = new ArrayList<Integer>();
+			while(size > 0) {
+				TreeNode cur = queue.poll();
+				curLayer.add(cur.key);
+				if(cur.left != null) {
+					queue.offer(cur.left);
+				}
+				if(cur.right != null) {
+					queue.offer(cur.right);
+				}
+				size--;
+			}
+			result.add(curLayer);
+			
+		}
+		return result;
+	}
+}
---
+++ src/Class05_BFS_Graph/KthSmallestNumberInSortedMatrix.java
@@ -0,0 +1,10 @@
+package Class05_BFS_Graph;
+
+public class KthSmallestNumberInSortedMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
