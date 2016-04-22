dce5142c37765f39e6202371fc4ad22e2a291223
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-04-22 02:37:17 -0400

    apr.21

--- src/Class11_DP1/ArrayHopper1.java
+++ src/Class11_DP1/ArrayHopper1.java
@@ -16,8 +16,24 @@ package Class11_DP1;
  */
 public class ArrayHopper1 {
 	public boolean canJump(int[] array) {
-		// Write your solution here.
-		return false;
+		if (array.length == 1) {
+			return true;
+		}
+		boolean[] dp = new boolean[array.length];
+		dp[array.length - 1] = true;
+		for (int i = dp.length - 2; i >= 0 ; i--) {
+			if (i + array[i] >= array.length - 1) {
+				dp[i] = true;
+			} else {
+				for(int j = array[i]; j > 0; j--) {
+					if (dp[i + j]) {
+						dp[i] = true;
+						break;
+					}
+				}
+			}
+		}
+		return dp[0];
 	}
 
 	public static void main(String[] args) {
