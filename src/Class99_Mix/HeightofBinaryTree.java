c26b11b93026f3b9b07a9d960dce382d41e6e8b7
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-04-23 02:58:20 -0400

    apr.22 - height

--- src/Class99_Mix/HeightofBinaryTree.java
+++ src/Class99_Mix/HeightofBinaryTree.java
@@ -1,7 +1,14 @@
 package Class99_Mix;
 
-import Class05_BFS_Graph.TreeNode;
-
+class TreeNode {
+	public TreeNode left;
+	public TreeNode right;
+	public int value;
+	public int height;
+	public TreeNode(int v) {
+		value = v;
+	}
+}
 public class HeightofBinaryTree {
 	public int findHeight(TreeNode root) {
 		if (root == null) {
@@ -10,6 +17,14 @@ public class HeightofBinaryTree {
 		int left = findHeight(root.left);
 		int right = findHeight(root.right);
 		return Math.max(left, right) + 1;
+	} 
+	public int getHeight(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftHeight = getHeight(root.left);
+		int rightHeight = getHeight(root.right);
+		return Math.max(leftHeight, rightHeight) + 1;
 	}
 
 	public static void main(String[] args) {
